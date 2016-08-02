77dbd9c7e8d93977865188d1955fdcc08982401d
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-08-02 11:23:11 -0500

    username/email register length

--- summer_in_chicago_project/app/controllers/users_controller.rb
+++ summer_in_chicago_project/app/controllers/users_controller.rb
@@ -2,24 +2,55 @@ require 'bcrypt'
 
 class UsersController < ApplicationController
  		
-  get '/?' do
- 	 erb :event
-  end
 
   get '/register/?' do
     erb :register
   end
 
-  post '/register' do
+  post '/register/?' do
     password = BCrypt::Password.create(params['password'])
+    if params['username'].length > 4 && params['email'].length >  6
+      user = User.create username: params['username'], email: params['email'], password: password
 
- 	  user = User.create username: params['username'], email: params['email'], password: password
-    if user
-      'User was added'
-       p user
-       erb :event
-    else
-      'Error'
+      if user 
+        'User was added'
+         p user
+         erb :event
+      else
+        'Error'
+      end
+    else 
+      'Username or Email not long enough' 
     end
+
+ 	  
+  end  
+
+  get '/login/?' do
+    erb :login      
   end
+
+  post '/login/?' do
+    user = User.find_by username: params['username']
+
+    if user
+      password = BCrypt::Password.new(user.password)
+
+      if password == params['password']
+        'Welcome Back'
+      else
+        'Incorrect Username or Password'
+      end
+    else
+      'Username could not be found'
+    end
+    
+  end
+
+  get '/' do
+    erb :event
+  end
+
+
+
 end
