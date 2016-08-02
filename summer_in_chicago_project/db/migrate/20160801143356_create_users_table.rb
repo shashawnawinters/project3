a84f10262620e6937810a487f4fb16fc987ad03e
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-08-02 09:22:48 -0500

    related tables

--- summer_in_chicago_project/db/migrate/20160801143356_create_users_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersTable < ActiveRecord::Migration
-  def change
-    create_table :users do |table|
-      table.string :username
-      table.string :password
-      table.string :email
-    end
-  end
-end
