1510c016040cedf4f60b10d948c71db36707ddfd
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-08-02 10:30:37 -0500

    register users

--- summer_in_chicago_project/app/controllers/users_controller.rb
+++ summer_in_chicago_project/app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
+require 'bcrypt'
+
 class UsersController < ApplicationController
  		
   get '/?' do
  	 erb :event
   end
 
+  get '/register/?' do
+    erb :register
+  end
+
+  post '/register' do
+    password = BCrypt::Password.create(params['password'])
+
+ 	  user = User.create username: params['username'], email: params['email'], password: password
+    if user
+      'User was added'
+       p user
+       erb :event
+    else
+      'Error'
+    end
+  end
 end
