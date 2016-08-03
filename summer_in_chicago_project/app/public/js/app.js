b5c2ec275154bd84e0754bd160264b65afb46473
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-08-03 11:02:32 -0500

    Wed 11:02 AM

--- summer_in_chicago_project/app/public/js/app.js
+++ summer_in_chicago_project/app/public/js/app.js
@@ -5,13 +5,18 @@ $(document).ready(function() {
 })
 var ajax = {
         type: 'get',
-        url: 'https://www.eventbriteapi.com/v3/events/search/?token=4UFJIOZKIUA72WMHQGQI',
+        url: 'https://www.eventbriteapi.com/v3/events/search/',
         beforeSend: function (request) {
           request.setRequestHeader("Authorization", "Bearer 4UFJIOZKIUA72WMHQGQI")
         },
         dataType: 'json',
         success: function(data) {
-          console.log(data);
+          console.log('<----------------this is the data-------------->'),
+          console.log(data),
+          console.log('<----------------this is an event object\'s name-------------->'),
+          console.log(data.events[0].name.text)
+          console.log('<----------------this is an event object-------------->'),
+          console.log(data.events[0])
         },
         error: function(err) {
           console.log(err);
