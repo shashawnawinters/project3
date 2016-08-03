d157095dd3e5abde040ddd83b19a93ec6719c38f
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-08-03 13:51:22 -0500

    Wed 1:51PM

--- summer_in_chicago_project/app/public/js/app.js
+++ summer_in_chicago_project/app/public/js/app.js
@@ -1,27 +1,36 @@
 console.log('working')
+results = document.getElementById('results')
 
-$(document).ready(function() {
-  $.ajax(ajax)
+$('#search').on("click", function(event) {
+  event.preventDefault(),
+  $.ajax({
+    type: 'get',
+    url: 'https://www.eventbriteapi.com/v3/events/search/',
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", "Bearer 4UFJIOZKIUA72WMHQGQI")
+    },
+    dataType: 'json',
+    success: function(data) {
+      var events = data.events;
+      for (var i = 0; i < events.length; i++) {
+        $('#results').append('<li><a href="#">' + events[i].name.text + '</li></a>')
+      }
+      console.log('<----------------this is the data-------------->'),
+      console.log(data),
+      console.log('<----------------this is an event object\'s name-------------->'),
+      console.log(data.events[0].name.text)
+      console.log('<----------------this is an event object-------------->'),
+      console.log(data.events[0]),
+      $('body').append(data);
+      $('#results').show();
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
 })
-var ajax = {
-        type: 'get',
-        url: 'https://www.eventbriteapi.com/v3/events/search/',
-        beforeSend: function (request) {
-          request.setRequestHeader("Authorization", "Bearer 4UFJIOZKIUA72WMHQGQI")
-        },
-        dataType: 'json',
-        success: function(data) {
-          console.log('<----------------this is the data-------------->'),
-          console.log(data),
-          console.log('<----------------this is an event object\'s name-------------->'),
-          console.log(data.events[0].name.text)
-          console.log('<----------------this is an event object-------------->'),
-          console.log(data.events[0])
-        },
-        error: function(err) {
-          console.log(err);
-        }
-}
+
+
 
 
 
