22ddcdf89782fb2e5b642b7e0ffa521197799809
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-08-03 01:54:54 -0500

    Tues 1:54

--- summer_in_chicago_project/app/controllers/users_controller.rb
+++ summer_in_chicago_project/app/controllers/users_controller.rb
@@ -5,31 +5,6 @@ class UsersController < ApplicationController
 
   get '/register/?' do
     erb :register
-  end
-
-  post '/register/?' do
-    password = BCrypt::Password.create(params['password'])
-    if params['username'].length > 1 && params['email'].length >  6
-      user  = User.find_by username: params['username']
-      email = User.find_by email: params['email']
-      if user || email
-        'Username already taken or Email already registered'
-      else
-        user = User.create username: params['username'], email: params['email'], password: password
-        if user 
-          'User was added'
-          session[:is_logged_in] = true
-          session[:user_id] = user.id
-          p session
-          user.to_json
-          
-        else
-          'Error'
-        end
-      end
-    else 
-      'Username or Email not long enough' 
-    end  
   end  
 
   get '/login/?' do
@@ -64,16 +39,40 @@ class UsersController < ApplicationController
 
   get '/membersonly/?' do
     p session
-    if session[:is_logged_in]  
+    if session[:is_logged_in]
       'Hello ' 
     else
-      p session
       'Unauthorized Access'
     end
   end
 
   get '/' do
     erb :event
+  end
+
+  post '/register/?' do
+    password = BCrypt::Password.create(params['password'])
+    if params['username'].length > 1 && params['email'].length >  6
+      user  = User.find_by username: params['username']
+      email = User.find_by email: params['email']
+      if user || email
+        'Username already taken or Email already registered'
+      else
+        user = User.create username: params['username'], email: params['email'], password: password
+        if user 
+          'User was added'
+          session[:is_logged_in] = true
+          session[:user_id] = user.id
+          p session
+          user.to_json
+          
+        else
+          'Error'
+        end
+      end
+    else 
+      'Username or Email not long enough' 
+    end  
   end
 
 
--- summer_in_chicago_project/app/public/js/app.js
+++ summer_in_chicago_project/app/public/js/app.js
@@ -1,1 +1,22 @@
 console.log('working')
+
+$(document).ready(function() {
+  $.ajax(ajax)
+})
+var ajax = {
+        type: 'get',
+        url: 'https://www.eventbriteapi.com/v3/events/search/?token=4UFJIOZKIUA72WMHQGQI',
+        beforeSend: function (request) {
+          request.setRequestHeader("Authorization", "Bearer 4UFJIOZKIUA72WMHQGQI")
+        },
+        dataType: 'json',
+        success: function(data) {
+          console.log(data);
+        },
+        error: function(err) {
+          console.log(err);
+        }
+}
+
+
+
