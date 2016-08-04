bcd051e8195415ba2abb930e25611ebcdf713971
shashawnawinters <shashawnawinters>
2016-08-04 11:43:23 -0500

    db fix

--- summer_in_chicago_project/db/schema.rb
+++ summer_in_chicago_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803193539) do
+ActiveRecord::Schema.define(version: 20160804164156) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id"
